ca8c6f780b37e122c529294777b1c3ebc541228c
David ANDRIASON <davidandriason7@gmail.com>
2021-05-07 20:37:15 +0200

    Add of the JS to make the website more dynamic

--- script/script.js
+++ script/script.js
@@ -2,13 +2,12 @@ const btnToggle = document.getElementById("btnToggle");
 const nav = document.querySelector(".navbar__nav");
 const navbar = document.querySelector(".navbar");
 const links = document.querySelectorAll(".nav__link");
-const about = document.getElementById("about");
-const project = document.getElementById("project");
-const contact = document.getElementById("contact");
+const sections = document.querySelectorAll(".section");
 
 // appartion of the nav when we click on the toggleBtn
 btnToggle.addEventListener("click", () => {
   nav.classList.toggle("navbar__nav--active");
+  btnToggle.classList.toggle("btn-toggle--active")
 });
 
 // Apparition of fixed navbar
@@ -16,54 +15,69 @@ window.addEventListener("scroll", function () {
     const scrollHeight = window.pageYOffset;
     const navHeight = navbar.getBoundingClientRect().height;
   
-    if (scrollHeight > navHeight) {
+    if (scrollHeight > navHeight ) {
       navbar.classList.add("navbar__fixed");
     } else {
       navbar.classList.remove("navbar__fixed");
     }
   });
 
-// // nav links active
-// window.addEventListener("scroll", function () {
-//     const scrollHeight = window.pageYOffset;
-//     const aboutTop = about.offsetTop
 
-  
-//     if (scrollHeight > offsetTop) {
-//       navbar.classList.add("navbar__fixed");
-//     } else {
-//       navbar.classList.remove("navbar__fixed");
-//     }
-//   });
-
-
-// smooth scroll
+  // smooth scroll
 links.forEach((link) => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault()
-        // get the element of the links anchor
-        const id = e.currentTarget.getAttribute('href')
-        const element = document.querySelector(id)
+  link.addEventListener('click', (e) => {
+      e.preventDefault()
+      // get the element of the links anchor
+      const id = e.currentTarget.getAttribute('href').slice(1)
+      const element = document.getElementById(id)
 
-        //calculate the position of element
-        const navbarHeight = navbar.getBoundingClientRect().height
-        const navHeight = nav.getBoundingClientRect().height
-        const navFixed = navbar.classList.contains("navbar__fixed")
+      //calculate the position of element
+      const navbarHeight = navbar.getBoundingClientRect().height
+      const navHeight = nav.getBoundingClientRect().height
+      const navFixed = navbar.classList.contains("navbar__fixed")
 
-        let position = element.offsetTop - navHeight;
-         if (!navFixed) {
-           position = position ;
-         }
-         if (navHeight > 50 ) {
-           position = position + navHeight - 40;
-         }
-         window.scrollTo({
-           left: 0,
-           top: position,
-         });
+      let position = element.offsetTop - navHeight;
+       if (!navFixed) {
+         position = position ;
+       }
+       if (navHeight > 50 ) {
+         position = position + navHeight - 40;
+       }
+       window.scrollTo({
+         left: 0,
+         top: position,
+       });
 
-         if (nav.classList.contains("navbar__nav--active")) {
-             nav.classList.remove("navbar__nav--active")
-         }
-    })
+       if (nav.classList.contains("navbar__nav--active")) {
+           nav.classList.remove("navbar__nav--active")
+       }
+
+       if (btnToggle.classList.contains("btn-toggle--active")) {
+        btnToggle.classList.remove("btn-toggle--active")
+    }
+  })
 })
+
+// // nav links active
+window.addEventListener("scroll", function () {
+  let current = "";
+  const scrollHeight = window.pageYOffset;
+  const navHeight = nav.getBoundingClientRect().height;
+  
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop
+
+    if(scrollHeight >= sectionTop - 200){
+      current = section.getAttribute("id")
+    }
+  })
+
+  links.forEach((link) => {
+    link.classList.remove('nav__link--active')
+    if (link.classList.contains(`nav__link--${current}`)) {
+      link.classList.add('nav__link--active')
+    }
+  })
+});
+
+
